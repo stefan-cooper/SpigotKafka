2044306cc7b0620df3b0120bbc989fc8832c0ecf
Stefan Cooper <stefan.cooper27@gmail.com>
2021-12-19 02:46:17 +0000

    Allow multiple doorbells

---
+++ src/main/java/com/stefancooper/KafkaMinecraft/Doorbell.java
@@ -0,0 +1,34 @@
+package com.stefancooper.KafkaMinecraft;
+
+import org.bukkit.Location;
+
+import java.util.UUID;
+
+public class Doorbell {
+
+    private Location location;
+    private boolean motionDetected;
+    private String id;
+
+    public Doorbell (String playerName, Location location, boolean motionDetected) {
+        this.location = location;
+        this.motionDetected = motionDetected;
+        this.id = playerName + "-" + UUID.randomUUID().toString();
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public boolean getMotionDetected() {
+        return motionDetected;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setMotionDetected(boolean motionDetected) {
+        this.motionDetected = motionDetected;
+    }
+}
